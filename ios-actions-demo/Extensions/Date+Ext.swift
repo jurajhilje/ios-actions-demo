a1ce9193708fd8f8e2ece226620a5e8de18f317a
Juraj Hilje <juraj@ivpn.net>
2020-09-16 15:11:54 +0200

    Update Date+Ext.swift

--- ios-actions-demo/Extensions/Date+Ext.swift
+++ ios-actions-demo/Extensions/Date+Ext.swift
@@ -12,7 +12,6 @@ extension Date {
     
     func days(between otherDate: Date) -> Int {
         let calendar = Calendar.current
-
         let startOfSelf = calendar.startOfDay(for: self)
         let startOfOther = calendar.startOfDay(for: otherDate)
         let components = calendar.dateComponents([.day], from: startOfSelf, to: startOfOther)
